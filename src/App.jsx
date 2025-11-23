fa74ae8f8773d71ed211f2cd1e3b616336aa5f08
Supun Anjana <supuna793@gmail.com>
2025-11-23 10:57:13 +0530

    Added MongoDB icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Navbar } from "./components/Navbar";
-import { Hero } from "./components/Hero"; // 1. Hero එක import කරගත්තා
+import { Hero } from "./components/Hero";
+import { TechStack } from "./components/TechStack"; 
 
 function App() {
   return (
     <div className="bg-dark min-h-screen text-white font-sans">
       <Navbar />
-      
-      {/* 2. Hero Component එක මෙතනට දැම්මා */}
       <Hero />
-      
+      <TechStack />  {/* <-- මෙතනත් නම මාරු කරන්න */}
     </div>
   );
 }
