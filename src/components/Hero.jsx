dbdbf87704c31ec4308a0e5c5cc11ac5103ee20a
Supun Anjana <supuna793@gmail.com>
2025-12-14 22:02:49 +0530

    Update opacity of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ export const Hero = () => {
         
         {/* Left Side: Text Content */}
         <motion.div
-          initial={{ opacity: 0, x: -50 }}
+          initial={{ opacity: 1, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
         >
